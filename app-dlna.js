7e05269925f96bef63771cdac06957f09ec2f806
Yuki KAN <re@pixely.jp>
2013-01-17 04:30:36 +0900

    DLNA: とりあえずobject.container.storageFolderを表示してみる

--- app-dlna.js
+++ app-dlna.js
@@ -34,10 +34,11 @@ process.on('SIGQUIT', function() {
 });
 
 // 追加モジュールのロード
-var chinachu  = require('chinachu-common');
-var SSDP      = require('chinachu-ssdp').SSDP;
-var xml2js    = require('xml2js');
-var xmlParser = new xml2js.Parser();
+var chinachu   = require('chinachu-common');
+var SSDP       = require('chinachu-ssdp').SSDP;
+var xml2js     = require('xml2js');
+var xmlParser  = new xml2js.Parser();
+var dateFormat = require('dateformat');
 
 // 設定の読み込み
 var config = JSON.parse( fs.readFileSync(CONFIG_FILE, 'ascii') );
@@ -93,9 +94,7 @@ mediaServer.addUSN('urn:schemas-upnp-org:service:ContentDirectory:1');
 mediaServer.addUSN('urn:schemas-upnp-org:service:ConnectionManager:1');
 
 mediaServer.on('advertise-alive', function (head) {
-	if (head.LOCATION === "http://192.168.1.101:20772/device.xml") return;
-	
-	util.log(JSON.stringify(head, null, '  '));
+	//util.log(JSON.stringify(head, null, '  '));
 });
 
 mediaServer.on('advertise-bye', function (head) {
@@ -114,6 +113,7 @@ var app = http.createServer(httpServer);
 app.listen(port, '0.0.0.0');
 
 function httpServer(req, res) {
+	
 	if (req.method === 'GET') {
 		
 		httpServerMain(req, res, null);
@@ -161,16 +161,12 @@ function httpServerMain(req, res, query) {
 	
 	// エラーレスポンス用
 	var resErr = function _resErr(code) {
+		
 		res.writeHead(code, {'content-type': 'text/plain'});
+		
 		switch (code) {
 			case 400:
 				res.write('400 Bad Request\n');
-				break;
-			case 402:
-				res.write('402 Payment Required\n');
-				break;
-			case 401:
-				res.write('401 Unauthorized\n');
 				break;
 			case 403:
 				res.write('403 Forbidden\n');
@@ -178,69 +174,24 @@ function httpServerMain(req, res, query) {
 			case 404:
 				res.write('404 Not Found\n');
 				break;
-			case 405:
-				res.write('405 Method Not Allowed\n');
-				break;
-			case 406:
-				res.write('406 Not Acceptable\n');
-				break;
-			case 407:
-				res.write('407 Proxy Authentication Required\n');
-				break;
-			case 408:
-				res.write('408 Request Timeout\n');
-				break;
-			case 409:
-				res.write('409 Conflict\n');
-				break;
-			case 410:
-				res.write('410 Gone\n');
-				break;
-			case 411:
-				res.write('411 Length Required\n');
-				break;
-			case 412:
-				res.write('412 Precondition Failed\n');
-				break;
-			case 413:
-				res.write('413 Request Entity Too Large\n');
-				break;
-			case 414:
-				res.write('414 Request-URI Too Long\n');
-				break;
-			case 415:
-				res.write('415 Unsupported Media Type\n');
-				break;
-			case 416:
-				res.write('416 Requested Range Not Satisfiable\n');
-				break;
-			case 417:
-				res.write('417 Expectation Failed\n');
-				break;
 			case 429:
 				res.write('429 Too Many Requests\n');
 				break;
-			case 451:
-				res.write('451 Unavailable For Legal Reasons\n');
-				break;
 			case 500:
 				res.write('500 Internal Server Error\n');
-				break;
-			case 501:
-				res.write('501 Not Implemented\n');
-				break;
-			case 502:
-				res.write('502 Bad Gateway\n');
 				break;
 			case 503:
 				res.write('503 Service Unavailable\n');
 				break;
 		}
+		
 		res.end();
+		
 		log(code);
 	}
 	
 	var writeHead = function _writeHead(code) {
+		
 		var type = 'text/plain';
 		
 		if (ext === 'xml')  { type = 'text/xml'; }
@@ -252,6 +203,7 @@ function httpServerMain(req, res, query) {
 		if (ext === 'png')  { type = 'image/png'; }
 		if (ext === 'gif')  { type = 'image/gif'; }
 		if (ext === 'jpg')  { type = 'image/jpeg'; }
+		if (ext === 'bmp')  { type = 'image/bmp'; }
 		
 		res.writeHead(code, {
 			'content-type': type,
@@ -260,20 +212,91 @@ function httpServerMain(req, res, query) {
 	}
 	
 	var responseStatic = function _responseStatic() {
+		
 		if (fs.existsSync(filename) === false) return resErr(404);
 		
 		fs.readFile(filename, function(err, data) {
+			
 			if (err) return resErr(500);
 			
 			writeHead(200);
 			res.end(data);
+			
 			log(200);
+			
 			return;
 		});
 	}
 	
 	switch (req.url) {
 		case '/cd-control.xml':
+			
+			var browse = query['s:Envelope']['s:Body'][0]['u:Browse'][0];
+			
+			var objectId = browse.ObjectID[0];
+			var start    = browse.StartingIndex[0];
+			var count    = browse.RequestedCount[0];
+			
+			var envelope = '';
+			var response = '';
+			var matched  = 0;
+			var results  = [];
+			var result   = '';
+			
+			envelope += '<?xml version="1.0"?>\r\n';
+			envelope += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">';
+			envelope += '<s:Body>';
+			envelope += '<u:BrowseResponse xmlns:u="urn:schemas-upnp-org:service:ContentDirectory:1">';
+			envelope += '::response::';
+			envelope += '</u:BrowseResponse>';
+			envelope += '</s:Body>';
+			envelope += '</s:Envelope>';
+			
+			switch (objectId) {
+				case '0':
+					
+					matched = 1;
+					
+					results.push({
+						id        : 'C1',
+						class     : 'container',
+						parentId  : '0',
+						title     : 'Recorded',
+						childCount: recorded.length
+					});
+					
+					break;
+			}
+			
+			result += '<DIDL-Lite xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/" xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/">';
+			
+			results.forEach(function(a, i) {
+				
+				if (a.class === 'container') {
+					result += '<container id="' + a.id + '" restricted="1" parentID="' + a.parentId + '" childCount="' + a.childCount + '">';
+					result += '<dc:title>' + a.title + '</dc:title>';
+					result += '<dc:date>' + dateFormat(new Date(), 'yyyy-mm-dd') + '</dc:date>';
+					result += '<upnp:class>object.container.storageFolder</upnp:class>';
+					result += '</container>';
+				}
+				
+				
+			});
+			
+			result += '</DIDL-Lite>';
+			
+			response += '<Result>' + result.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</Result>';
+			
+			response += '<NumberReturned>' + results.length + '</NumberReturned>';
+			response += '<TotalMatches>' + matched + '</TotalMatches>';
+			response += '<UpdateID>' + new Date().getTime() + '</UpdateID>';
+			
+			writeHead(200);
+			res.end(envelope.replace('::response::', response));
+			
+			util.log(envelope.replace('::response::', response));
+			
+			log(200);
 			
 			break;
 		
