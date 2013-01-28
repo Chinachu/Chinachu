3cc1ae7d10ca7f09703655cd1260b1b066e18346
Yuki KAN <re@pixely.jp>
2013-01-28 22:53:37 +0900

    WUI/API: asfコンテナをサポート (recording, recorded)

--- api/script-recorded-program-watch.vm.js
+++ api/script-recorded-program-watch.vm.js
@@ -66,6 +66,7 @@
 		case 'f4v':
 		case 'flv':
 		case 'webm':
+		case 'asf':
 			response.head(200);
 			
 			util.log('[streamer] streaming: ' + program.recorded);
@@ -95,11 +96,16 @@
 				
 				if (vcodec === 'copy') { vcodec = 'libvpx'; }
 				if (acodec === 'copy') { acodec = 'libvorbis'; }
+			} else if (request.type === 'asf') {
+				format = 'asf';
+				
+				if (vcodec === 'copy') { vcodec = 'wmv2'; }
+				if (acodec === 'copy') { acodec = 'libfdk_aac'; }
 			}
 			
 			var args = [];
 			
-			args.push('-v', '0');
+			if (!request.query.debug) args.push('-v', '0');
 			
 			args.push('-ss', (parseInt(start, 10) - 1) + '');
 			
