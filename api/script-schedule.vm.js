1e90ab9a17c0467244bc0d51a3214deab1d5f95c
Yuki KAN <re@pixely.jp>
2013-09-25 15:44:49 +0900

    WUI/API: deflate対応など

--- api/script-schedule.vm.js
+++ api/script-schedule.vm.js
@@ -1,10 +1,35 @@
-(function() {
-	
-	switch (request.method) {
-		case 'GET':
-			response.head(200);
-			response.end(JSON.stringify(data.schedule, null, '  '));
-			return;
-	}
+var mtime = fs.statSync(define.SCHEDULE_DATA_FILE).mtime;
 
-})();
+response.setHeader('last-modified', new Date(mtime).toUTCString());
+
+if (request.headers['if-modified-since'] && request.headers['if-modified-since'] === new Date(mtime).toUTCString()) {	
+	response.head(304);
+	response.end();
+} else {
+	var input = JSON.stringify(data.schedule);
+	
+	switch (request.encoding) {
+	case 'deflate':
+		response.setHeader('content-encoding', 'deflate');
+		zlib.deflate(input, function (err, buffer) {
+			if (!err) {
+				response.setHeader('content-length', buffer.length);
+				response.head(200);
+				if (request.method === 'GET') {
+					response.end(buffer);
+				} else {
+					response.end();
+				}
+			}
+		});
+		break;
+	default:
+		response.setHeader('content-length', input.length);
+		response.head(200);
+		if (request.method === 'GET') {
+			response.end(input);
+		} else {
+			response.end();
+		}
+	}
+}
--- app-wui.js
+++ app-wui.js
@@ -25,6 +25,7 @@ var url           = require('url');
 var querystring   = require('querystring');
 var vm            = require('vm');
 var os            = require('os');
+var zlib          = require('zlib');
 
 // ディレクトリチェック
 if (!fs.existsSync('./data/') || !fs.existsSync('./log/') || !fs.existsSync('./web/')) {
@@ -603,6 +604,16 @@ function httpServerMain(req, res, query) {
 				res.emit('end');
 			};
 			
+			var acceptEncoding = req.headers['accept-encoding'];
+			if (!acceptEncoding) { acceptEncoding = ''; }
+			var encoding = '';
+			
+			if (acceptEncoding.match(/deflate/)) {
+				encoding = 'deflate';
+			}/* else if (acceptEncoding.match(/gzip/)) {
+				encoding = 'gzip';
+			}*/
+			
 			var sandbox = {
 				request      : req,
 				response     : res,
@@ -612,6 +623,7 @@ function httpServerMain(req, res, query) {
 				util         : util,
 				child_process: child_process,
 				Buffer       : Buffer,
+				zlib         : zlib,
 				chinachu     : chinachu,
 				config       : config,
 				define: {
@@ -643,11 +655,12 @@ function httpServerMain(req, res, query) {
 			
 			var isClosed = false;
 			
-			sandbox.request.query  = query;
-			sandbox.request.param  = param;
-			sandbox.request.type   = ext;
-			sandbox.response.head  = writeHead;
-			sandbox.response.error = function(code) {
+			sandbox.request.query    = query;
+			sandbox.request.param    = param;
+			sandbox.request.type     = ext;
+			sandbox.request.encoding = encoding;
+			sandbox.response.head    = writeHead;
+			sandbox.response.error   = function(code) {
 				
 				isClosed = true;
 				
