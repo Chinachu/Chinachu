5e97e0dc47fdba5fad0c007f68cc58b3a72727a5
Yuki KAN <re@pixely.jp>
2015-09-26 13:50:03 +0900

    ストレージ使用量: グラフサイズを固定する

--- web/page/dashboard/storage.js
+++ web/page/dashboard/storage.js
@@ -1,39 +1,39 @@
 P = Class.create(P, {
-	
+
 	init: function _initPage() {
-		
+
 		this.view.content.className = 'loading';
-		
+
 		this.onNotify = this.refresh.bindAsEventListener(this);
 		document.observe('chinachu:storage', this.onNotify);
-		
+
 		this.draw();
-		
+
 		return this;
 	}
 	,
 	refresh: function() {
-		
+
 		this.draw();
-		
+
 		return this;
 	}
 	,
 	deinit: function _deinit() {
-		
+
 		document.stopObserving('chinachu:storage', this.onNotify);
-		
+
 		return this;
 	}
 	,
 	draw: function _draw() {
-		
+
 		this.view.content.className = '';
 		this.view.content.update();
-		
+
 		var chart = dc.pieChart(this.view.content);
 		d3.json("./api/storage.json", function(error, data) {
-			
+
 			function readableFilesize(size) {
 				var postfix = ["B","KB","MB","GB","TB","PB"];
 				var i = 0;
@@ -46,7 +46,7 @@ P = Class.create(P, {
 				}
 				return (size/base).toFixed(1) + postfix[i];
 			}
-			
+
 			var pickBgColor = function(klass) {
 				var x = new Element('span');
 				x.setAttribute('class', klass);
@@ -56,12 +56,12 @@ P = Class.create(P, {
 				if( d3bgcol['3'] === undefined ) d3bgcol['3'] = 1.0; //d3.rgb has sloppy rgba support.
 				return d3bgcol;
 			}.bind(this);
-			
+
 			var d3rgb2rgba = function(h) { return "rgba("+h['r']+","+h['g']+","+h['b']+","+h['3']+")"; }
 			var d3rgb_transparent = function(h, tr) { h['3'] = tr; return h; }
-			
-			
-			
+
+
+
 			var catList   = ['RECORDED', 'FREE', 'USED BY OTHER PROGRAMS'].map(function(x) { return x.__();} );
 			var colorList = [	pickBgColor("bg-chinachu"),
 						d3rgb_transparent( pickBgColor("bg-chinachu"), 0.4),
@@ -69,24 +69,24 @@ P = Class.create(P, {
 					].map(function(a) { return d3rgb2rgba(a) } );
 			var colorMap = catList.reduce( function(p, c, i, s) { p[c] = colorList[i]; return p; }, {});
 			var orderMap = catList.reduce( function(p, c, i, s) { p[c] = i+1; return p; }, {});
-			
-			var w = this.view.content.getWidth();
-			var h = this.view.content.getHeight();
+
+			var w = 400;
+			var h = 400;
 			var width = Math.min(w,h) * 0.8;
 			var height = width;
 			var radius = width * 0.4;
-			
+
 			var storageUsage = [
 				{ category: catList[0], value: data.recorded },
 				{ category: catList[1], value: data.avail},
 				{ category: catList[2], value: data.used - data.recorded}
 			];
-			
+
 			var ndx = crossfilter(storageUsage);
 			var category = ndx.dimension(function(d) {return d.category;});
 			var valueGroup = category.group().reduceSum(function(d) {return d.value;});
-			
-			
+
+
 			chart
 			 .width(width)
 			 .height(height)
@@ -98,10 +98,10 @@ P = Class.create(P, {
 			 .label( function(d) { return d.data.key + ':' + readableFilesize(d.data.value); } )
 			 .ordering( function(d) { return orderMap[d.key]; })
 			 .title( function(d) { return readableFilesize(d.data.value); } );
-			
+
 			chart.render();
 		}.bind(this));
-		
+
 		return this;
 	}
 });
