8ef726c276b0677cee48328df428cf633e9b935a
yutaka <shigure@refy.net>
2017-01-16 09:18:09 +0900

    Revert: Modifiy to library

--- web/lib/prototype.js
+++ web/lib/prototype.js
@@ -18,8 +18,7 @@ var Prototype = {
       Opera:          isOpera,
       WebKit:         ua.indexOf('AppleWebKit/') > -1,
       Gecko:          ua.indexOf('Gecko') > -1 && ua.indexOf('KHTML') === -1,
-      MobileSafari:   /Apple.*Mobile/.test(ua),
-      Android:        ua.indexOf('Linux; Android') > -1
+      MobileSafari:   /Apple.*Mobile/.test(ua)
     }
   })(),
 
