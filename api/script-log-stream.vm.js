fd4711d1c160c4d650140c78ffe105c13549a5e4
Yuki KAN <re@pixely.jp>
2017-08-20 14:22:55 +0900

    WUI/API: fix log file path #330 #332

--- api/script-log-stream.vm.js
+++ api/script-log-stream.vm.js
@@ -3,28 +3,39 @@
 	if (['wui', 'operator', 'scheduler'].indexOf(request.param.name) === -1) {
 		return response.error(404);
 	}
-	
-	var filename = './log/' + request.param.name;
-	
+
+	let filename = "";
+	switch (request.param.name) {
+		case "wui":
+			filename = define.WUI_LOG_FILE;
+			break;
+		case "scheduler":
+			filename = define.SCHEDULER_LOG_FILE;
+			break;
+		case "operator":
+			filename = define.OPERATOR_LOG_FILE;
+			break;
+	}
+
 	if (!fs.existsSync(filename)) {
 		response.head(204);
 		response.end('');
 		return;
 	}
-	
+
 	response.head(200);
-	
+
 	response.write(new Array(1024).join(' '));
-	
+
 	var tailf = child_process.spawn('tail', ['-f', '-n', '100', filename]);
 	children.push(tailf.pid);
-	
+
 	tailf.stdout.pipe(response);
-	
+
 	tailf.on('exit', function(code) {
 		response.end();
 	});
-	
+
 	request.on('close', function() {
 		tailf.kill('SIGKILL');
 	});
--- api/script-log.vm.js
+++ api/script-log.vm.js
@@ -3,23 +3,34 @@
 	if (['wui', 'operator', 'scheduler'].indexOf(request.param.name) === -1) {
 		return response.error(404);
 	}
-	
-	var filename = './log/' + request.param.name;
-	
+
+	let filename = "";
+	switch (request.param.name) {
+		case "wui":
+			filename = define.WUI_LOG_FILE;
+			break;
+		case "scheduler":
+			filename = define.SCHEDULER_LOG_FILE;
+			break;
+		case "operator":
+			filename = define.OPERATOR_LOG_FILE;
+			break;
+	}
+
 	if (!fs.existsSync(filename)) {
 		response.head(204);
 		response.end('');
 		return;
 	}
-	
+
 	response.head(200);
-	
+
 	fs.readFile(filename, function(err, data) {
 		if (err) {
 			util.log(err);
 			return response.error(500);
 		}
-		
+
 		response.end(data);
 	});
 
